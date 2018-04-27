46821d6d8934420461efcff40a6a5fccee0621c1
Lht223 <2684594460@qq.com>
2018-04-27 10:45:52 +0800

    update

---
+++ assets/scripts/Game.js
@@ -0,0 +1,97 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+        starPrefab: {
+            default: null,
+            type: cc.Prefab
+        },
+        maxStarDuration: 0,
+        minStarDuration: 0,
+        ground: {
+            default: null,
+            type: cc.Node
+        },
+        player: {
+            default: null,
+            type: cc.Node
+        },
+        scoreDisplay: {
+            default: null,
+            type: cc.Label
+        },
+        scoreAudio: {
+            default: null,
+            url: cc.AudioClip
+        }
+    },
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad: function () {
+        this.groundY = this.ground.y + this.ground.height/2;
+        this.timer = 0;
+        this.starDuration = 0;
+        this.spawnNewStar();
+        this.score = 0;
+    },
+
+    spawnNewStar: function() {
+        var newStar = cc.instantiate(this.starPrefab);
+        this.node.addChild(newStar);
+        newStar.setPosition(this.getNewStarPosition());
+        newStar.getComponent('Star').game = this;
+        this.starDuration = this.minStarDuration + cc.random0To1() * (this.maxStarDuration - this.minStarDuration);
+        this.timer = 0;
+    },
+
+    getNewStarPosition: function () {
+        var randX = 0;
+        var randY = this.groundY + cc.random0To1() * this.player.getComponent('Player').jumpHeight + 50;
+        var maxX = this.node.width/2;
+        randX = cc.randomMinus1To1() * maxX;
+        return cc.p(randX, randY);
+    },
+    update: function (dt) {
+        if (this.timer > this.starDuration) {
+            this.gameOver();
+            return;
+        }
+        this.timer += dt;
+    },
+
+    gainScore: function () {
+        this.score += 1;
+        this.scoreDisplay.string = 'Score: ' + this.score.toString();
+        cc.audioEngine.playEffect(this.scoreAudio, false);
+    },
+
+    gameOver: function () {
+        this.player.stopAllActions(); 
+        cc.director.loadScene('game');
+    }
+});
---
+++ library/imports/74/74f8b482-6e3a-473b-91d9-5e9e986d579e.js
@@ -0,0 +1,111 @@
+"use strict";
+cc._RF.push(module, '74f8bSCbjpHO5HZXp6YbVee', 'Player');
+// scripts/Player.js
+
+"use strict";
+
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        jumpHeight: 0,
+        jumpDuration: 0,
+        maxMoveSpeed: 0,
+        accel: 0,
+        jumpAudio: {
+            default: null,
+            url: cc.AudioClip
+        }
+    },
+    // foo: {
+    //     // ATTRIBUTES:
+    //     default: null,        // The default value will be used only when the component attaching
+    //                           // to a node for the first time
+    //     type: cc.SpriteFrame, // optional, default is typeof default
+    //     serializable: true,   // optional, default is true
+    // },
+    // bar: {
+    //     get () {
+    //         return this._bar;
+    //     },
+    //     set (value) {
+    //         this._bar = value;
+    //     }
+    // },
+    setJumpAction: function setJumpAction() {
+        var jumpUp = cc.moveBy(this.jumpDuration, cc.p(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+        var jumpDown = cc.moveBy(this.jumpDuration, cc.p(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+        var callback = cc.callFunc(this.playJumpSound, this);
+        return cc.repeatForever(cc.sequence(jumpUp, jumpDown, callback));
+    },
+
+    playJumpSound: function playJumpSound() {
+        cc.audioEngine.playEffect(this.jumpAudio, false);
+    },
+
+    setInputControl: function setInputControl() {
+        var self = this;
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed: function onKeyPressed(keyCode, event) {
+                switch (keyCode) {
+                    case cc.KEY.a:
+                        self.accLeft = true;
+                        self.accRight = false;
+                        break;
+                    case cc.KEY.d:
+                        self.accLeft = false;
+                        self.accRight = true;
+                        break;
+                }
+            },
+            onKeyReleased: function onKeyReleased(keyCode, event) {
+                switch (keyCode) {
+                    case cc.KEY.a:
+                        self.accLeft = false;
+                        break;
+                    case cc.KEY.d:
+                        self.accRight = false;
+                        break;
+                }
+            }
+        }, self.node);
+    },
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad: function onLoad() {
+        this.jumpAction = this.setJumpAction();
+        this.node.runAction(this.jumpAction);
+
+        this.accLeft = false;
+        this.accRight = false;
+        this.xSpeed = 0;
+
+        this.setInputControl();
+    },
+
+    update: function update(dt) {
+        if (this.accLeft) {
+            this.xSpeed -= this.accel * dt;
+        } else if (this.accRight) {
+            this.xSpeed += this.accel * dt;
+        }
+        if (Math.abs(this.xSpeed) > this.maxMoveSpeed) {
+            this.xSpeed = this.maxMoveSpeed * this.xSpeed / Math.abs(this.xSpeed);
+        }
+
+        this.node.x += this.xSpeed * dt;
+    }
+});
+
+cc._RF.pop();
